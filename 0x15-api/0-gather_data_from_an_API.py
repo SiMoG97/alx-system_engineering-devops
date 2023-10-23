052cd4dad34a071d0c0a8ad3fb0ef5c9e8d789a2
SiMoG97 <echaaranimohamed@gmail.com>
2023-10-23 17:18:01 +0100

    Task-0: adding 0-gather_data_from_an_API.py

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+script that, using this REST API, for a given employee ID,
+returns information about his/her TODO list progress.
+"""
+from sys import argv
+import requests
+
+
+if __name__ == "__main__":
+    try:
+        userRes = requests.get(f"https://jsonplaceholder.typicode.com/users/{argv[1]}")
+        tasksRes = requests.get(f"https://jsonplaceholder.typicode.com/todos?userId={argv[1]}")
+        
+        user = userRes.json()
+        tasks = tasksRes.json()
+        
+        name = user['name']
+
+        userRes.raise_for_status()
+        tasksRes.raise_for_status()
+        
+        tasksLen = len(tasks)
+        completedTasks = [task for task in tasks if task['completed']]
+        doneTasksNum = len(completedTasks) 
+
+        print(f"Employee {name} is done with tasks({doneTasksNum}/{tasksLen}):")
+        for task in completedTasks:
+            print(f"\t {task['title']}")
+
+    except requests.exceptions.RequestException as e:
+        print(f"Error code: {e.response.status_code}")
