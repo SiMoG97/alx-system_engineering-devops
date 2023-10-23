34be9e14f4fca3d5fcc607ed2e2e609011076517
SiMoG97 <echaaranimohamed@gmail.com>
2023-10-23 17:55:16 +0100

    Task-1: 1-export_to_CSV.py

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""
+script that exports a csv file
+"""
+import csv
+import requests
+from sys import argv
+
+
+if __name__ == "__main__":
+    try:
+        userRes = requests.get(
+            f"https://jsonplaceholder.typicode.com/users/{argv[1]}")
+        tasksRes = requests.get(
+            f"https://jsonplaceholder.typicode.com/todos?userId={argv[1]}")
+
+        user = userRes.json()
+        tasks = tasksRes.json()
+
+        userRes.raise_for_status()
+        tasksRes.raise_for_status()
+
+        completedTasks = [task for task in tasks if task['completed']]
+
+        id = user.get('id')
+        username = user.get('username')
+
+        with open(f"{argv[1]}.csv", mode="w", encoding="utf-8") as f:
+            Writer = csv.writer(
+                f, delimiter=",", quoting=csv.QUOTE_ALL)
+            for task in completedTasks:
+                taskStatus = task.get('completed')
+                title = task.get('title')
+
+                Writer.writerow([id, username, taskStatus, title])
+
+    except requests.exceptions.RequestException as e:
+        print(f"Error code: {e.response.status_code}")
